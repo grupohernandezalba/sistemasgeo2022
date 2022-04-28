f41696ada4eceadaf8026829256d0fdcff937ec8
Marco Antonio Hernández Alba <marco@grupohernandezalba.com>
2022-04-27 19:46:16 -0500

    se agrego 11 al 14

---
+++ 2022/firebase/practica1/index.js
@@ -0,0 +1,11 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js';
+
+const firebase = initializeApp({
+    apiKey: "AIzaSyDuDT8WC3_220mUGQLh7jDPnyHACniL8M0",
+    authDomain: "practica1-sistemasgeo.firebaseapp.com",
+    projectId: "practica1-sistemasgeo",
+    storageBucket: "practica1-sistemasgeo.appspot.com",
+    messagingSenderId: "1041887627099",
+    appId: "1:1041887627099:web:4d3e3a6ef738f7addefc7e"
+});
+
